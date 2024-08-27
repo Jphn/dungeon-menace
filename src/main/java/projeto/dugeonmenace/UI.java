6d4af366ea167c57079faacc3936147c4a330e24
Natan Pedrosa <natanpedrosateixeira10@gmail.com>
2024-08-27 14:12:56 -0300

    classes 19-22 and font update

--- src/main/java/projeto/dugeonmenace/UI.java
+++ src/main/java/projeto/dugeonmenace/UI.java
@@ -7,8 +7,11 @@ package projeto.dugeonmenace;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import projeto.dugeonmenace.entity.Entity;
 import projeto.dugeonmenace.objectsSprite.OBJ_Heart;
@@ -22,7 +25,7 @@ public class UI {
     GamePanel gp;
     Graphics2D g2;
 
-    Font arial_40, arial_80B;
+    Font pixelOperator;
 //    BufferedImage keyImage;
 
     //PLAYER STATUS HUD HEART IMAGES
@@ -46,11 +49,20 @@ public class UI {
     public UI(GamePanel gp) {
 
         this.gp = gp;
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80B = new Font("Arial", Font.BOLD, 80);
-        //       OBJ_Key key = new OBJ_Key(gp);
-//        keyImage = key.image;
-        //CREATE HUD OBJECT
+        
+        try {
+            InputStream is = getClass().getResourceAsStream("/fonts/PixelOperator.ttf");
+            pixelOperator = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // OBJ_Key key = new OBJ_Key(gp);
+        // keyImage = key.image;
+        
+        // CREATE HUD OBJECT
         Entity heart = new OBJ_Heart(gp);
         this.heartFull = heart.image;
         this.heartHalf = heart.image2;
@@ -61,7 +73,7 @@ public class UI {
     public void draw(Graphics2D g2) {
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+        g2.setFont(pixelOperator);
         g2.setColor(Color.white);
         /**
          * Essa região é diferente ao UI durante
